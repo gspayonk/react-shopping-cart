cbdb2a27a97244abcf136ef5fc6679cfcb8a41a4
gspayonk <grissobel.castro@gmail.com>
2019-10-06 02:37:14 -0400

    MVP

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+//context
+import ProductContext from './contexts/ProductContext';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
@@ -16,6 +19,7 @@ function App() {
 	};
 
 	return (
+		<ProductContext.Provider value={{products, addItem}}>
 		<div className="App">
 			<Navigation cart={cart} />
 
@@ -23,12 +27,7 @@ function App() {
 			<Route
 				exact
 				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
+				component = {Products}
 			/>
 
 			<Route
@@ -36,6 +35,7 @@ function App() {
 				render={() => <ShoppingCart cart={cart} />}
 			/>
 		</div>
+		</ProductContext.Provider>
 	);
 }
 
--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const Item = props => {
+
 	return (
+
 		<div className="shopping-cart_item">
 			<img src={props.image} alt={`${props.title} book`} />
-
 
 			<div>
 				<h1>{props.title}</h1>
