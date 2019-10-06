28e3e89ab77ca16e3cb9188e61c4b1bf24f418c5
gspayonk <grissobel.castro@gmail.com>
2019-10-06 18:55:56 -0400

    MVP Edits + Stretch

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import CartContext from '../contexts/CartContext';
+import {CartContext} from '../contexts/CartContext';
 import {useContext} from 'react';
 
 const Navigation = () => {
 
-	const cart = useContext(CartContext);
+	const {cart} = useContext(CartContext);
 
 	return (
 		<div className="navigation">
--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 const Item = props => {
 
+	const totalPrice = (props.price*props.quantity).toFixed(2)
+
 	return (
 
 		<div className="shopping-cart_item">
@@ -10,7 +12,8 @@ const Item = props => {
 			<div>
 				<h1>{props.title}</h1>
 				<p>$ {props.price}</p>
-				<button>Remove from cart</button>
+				<p>Quantity {props.quantity} (${totalPrice})</p>
+				<button onClick={() => props.removeItem(props.id)}>Remove from cart</button>
 			</div>
 		</div>
 	);
