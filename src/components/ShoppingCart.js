10960c2978832bb0c22bc6bd80892f1bbdcc97fa
gspayonk <grissobel.castro@gmail.com>
2019-10-07 16:26:06 -0400

    Stretch + Edits

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {useContext} from 'react';
-import CartContext from '../contexts/CartContext';
+import { useContext } from 'react';
+import { CartContext } from './../contexts/CartContext';
 
 // Components
 import Item from './ShoppingCartItem';
 
 const ShoppingCart = () => {
 
-	const cart = useContext(CartContext);
+	const { cart } = useContext(CartContext);
 
 	const getCartTotal = () => {
 
